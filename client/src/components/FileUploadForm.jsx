b22d11426fffd72d443059796dd0a9373aaf2ba9
omgardner <36763538+omgardner@users.noreply.github.com>
2023-05-28 21:08:04 +1000

    cleaned up package.json files on client and server, and made a partially working upload mechanism. In hindsight I should have separated this into two commits.

---
+++ client/src/components/FileUploadForm.jsx
@@ -0,0 +1,48 @@
+// initial code from: https://codefrontend.com/file-upload-reactjs/
+import { Typography } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
+
+function FileUploadForm() {
+  const url = "http://localhost:5000/upload"
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData()
+
+    const inputFilesElement = document.getElementById("files");
+
+    for (const file of inputFilesElement.files) {
+      formData.append("uploaded_files", file);
+    }
+
+    //console.log(inputFilesElement.files)
+
+    fetch(url,
+      {
+        method: "POST",
+        body: formData
+      }
+    ).then((result) => {
+      console.log('Success:', result);
+
+    })
+      .catch((error) => {
+        console.error('Error:', error);
+
+      });
+  }
+
+
+  return (
+    <form method="POST" enctype="multipart/form-data" onSubmit={handleSubmit}>
+      <input type="file" name="uploaded_files" multiple id="files" />
+      <input type="submit" />
+    </form>
+  );
+}
+
+
+
+
+
+export default FileUploadForm;
