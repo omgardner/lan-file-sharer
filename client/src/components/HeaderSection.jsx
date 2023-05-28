911a14cbe838ad920f3374ac9141f14a87473b6f
omgardner <36763538+omgardner@users.noreply.github.com>
2023-05-28 21:59:22 +1000

    Keeping the HeaderSection as it used to be for now

--- client/src/components/HeaderSection.jsx
+++ client/src/components/HeaderSection.jsx
@@ -4,14 +4,14 @@ import {Box,  Card, Typography, CardContent } from '@mui/material';
 const HeaderSection = () => {
     return (
         <Card>
-            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-            <Typography component="div" variant="h5">
+            <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">
                     Home Data Sharing
                 </Typography>
                 <Typography variant="subtitle1" color="text.secondary" component="div">
                     Transfer files between LAN devices
                 </Typography>
-            </Box>
+            </CardContent>
         </Card>
     );
 }
