a4f0aa45e894b3799da3ce8d1538d2566d05cc88
omgardner <36763538+omgardner@users.noreply.github.com>
2023-06-02 18:29:26 +1000

    Added another select box to change the sorting order of the file download list

--- client/src/components/DownloadSection.jsx
+++ client/src/components/DownloadSection.jsx
@@ -10,7 +10,6 @@ import { FileListContext, FileListDispatchContext } from './FileContext';
 
 const DownloadSection = () => {
   // handle initial retrieval of the data
-
   const fileMetadataArr = useContext(FileListContext)
   const dispatch = useContext(FileListDispatchContext)
 
@@ -25,19 +24,23 @@ const DownloadSection = () => {
   // reload the data the first time this component is created
   useEffect(reloadData, [])
 
-
   // allows for dynamic adjustment of the sorting order for the DownloadItem components
   const defaultSortBy = "uploadDate"
   const defaultIsAscending = false
 
   const [sortBy, setSortBy] = useState(defaultSortBy)
-  const [sortIsAscending, setsortIsAscending] = useState(defaultIsAscending)
+  const [isAscending, setIsAscending] = useState(defaultIsAscending)
+
+  function onSortChange (sortBy, isAscending) {
+    setSortBy(sortBy)
+    setIsAscending(isAscending)
+  }
 
   const sortingFunctionMapping = {
-    "uploadDate": (a, b) => (sortIsAscending ? a.uploadTimeEpochMs - b.uploadTimeEpochMs : b.uploadTimeEpochMs - a.uploadTimeEpochMs),
-    "filename": (a, b) => (sortIsAscending ? b.filename.localeCompare(a.filename) : a.filename.localeCompare(b.filename)),
-    "fileCategory": (a, b) => (sortIsAscending ? b.fileCategory.localeCompare(a.fileCategory) : a.fileCategory.localeCompare(b.fileCategory)),
-    "filesize": (a, b) => (sortIsAscending ? a.filesize - b.filesize : b.filesize - a.filesize)
+    "uploadDate": (a, b) => (isAscending ? a.uploadTimeEpochMs - b.uploadTimeEpochMs : b.uploadTimeEpochMs - a.uploadTimeEpochMs),
+    "filename": (a, b) => (isAscending ? b.filename.localeCompare(a.filename) : a.filename.localeCompare(b.filename)),
+    "fileCategory": (a, b) => (isAscending ? b.fileCategory.localeCompare(a.fileCategory) : a.fileCategory.localeCompare(b.fileCategory)),
+    "filesize": (a, b) => (isAscending ? a.filesize - b.filesize : b.filesize - a.filesize)
   }
 
   return (
@@ -45,8 +48,7 @@ const DownloadSection = () => {
       <Box sx={{ display: 'flex', flexDirection: 'row' }}>
         <h1>Download Section</h1>
         <button onClick={reloadData}>Refresh</button>
-        <DownloadSortAndFilter onChange={setSortBy} defaultSortBy={defaultSortBy} defaultIsAscending={defaultIsAscending} />
-        <p>DEBUG Sorting by: {sortBy}</p>
+        <DownloadSortAndFilter onSortChange={onSortChange} defaultSortBy={defaultSortBy} defaultIsAscending={defaultIsAscending} />
       </Box>
 
       {
--- client/src/components/DownloadSortAndFilter.jsx
+++ client/src/components/DownloadSortAndFilter.jsx
@@ -1,27 +1,46 @@
-import { MenuItem, Select } from '@mui/material'
+import { Box, MenuItem, Select } from '@mui/material'
 import React, { useState } from 'react'
-function DownloadSortAndFilter({ onChange, defaultSortBy}) {
+function DownloadSortAndFilter({ onSortChange, defaultSortBy, defaultIsAscending }) {
     const [sortBy, setSortBy] = useState(defaultSortBy)
+    const [isAscending, setIsAscending] = useState(defaultIsAscending)
     return (
-        <Select 
-            id="sortingBox" 
-            onChange={
-                (e) => {
-                    const newSortBy = e.target.value
-                    // changes the currently selected value
-                    setSortBy(newSortBy)
-                    // lifts up the value's state 
-                    onChange(newSortBy)
+        <Box>
+            <Select
+                onChange={
+                    (e) => {
+                        const newSortBy = e.target.value
+                        // changes the currently selected value
+                        setSortBy(newSortBy)
+                        // lifts up the value's state 
+                        onSortChange(newSortBy, isAscending)
+                    }
                 }
-            } 
-            value={sortBy}>
-            <MenuItem value={"uploadDate"}>Upload Date</MenuItem>
-            <MenuItem value={"filename"}>File Name</MenuItem>
-            <MenuItem value={"filesize"}>File Size</MenuItem>
-            <MenuItem value={"fileCategory"}>File Category</MenuItem>
-        </Select>
-        
-        
+                value={sortBy}>
+                <MenuItem value={"uploadDate"}>Upload Date</MenuItem>
+                <MenuItem value={"filename"}>File Name</MenuItem>
+                <MenuItem value={"filesize"}>File Size</MenuItem>
+                <MenuItem value={"fileCategory"}>File Category</MenuItem>
+            </Select>
+            <Select
+                onChange={
+                    (e) => {
+                        const newIsAscending = e.target.value
+                        console.log("newIsAscending")
+                        console.log(newIsAscending)
+                        // changes the currently selected value
+                        setIsAscending(newIsAscending)
+                        // lifts up the value's state 
+                        onSortChange(sortBy, newIsAscending)
+                    }
+                }
+                value={isAscending}>
+                <MenuItem value={true}>Ascending</MenuItem>
+                <MenuItem value={false}>Descending</MenuItem>
+            </Select>
+        </Box>
+
+
+
     )
 }
 
