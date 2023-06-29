a74806273d55c3154032a6aaae226ae551d1f0eb
omgardner <36763538+omgardner@users.noreply.github.com>
2023-06-29 22:54:51 +1000

    Added qrcode generator to the setup-env-files.js script. It isn't displayed anywhere on the app yet

--- setup-env-files.js
+++ setup-env-files.js
@@ -2,6 +2,8 @@ const fs = require('fs/promises')
 const os = require('os')
 const path = require('path')
 
+
+
 function getPrivateIPAddress() {
     // ensures that only a private IP address gets retrieved, returning null if it wasn't found.
     // note: the 'ip' npm package uses os.networkInterfaces() anyway, so it's cleaner to write this custom functionality myself.
@@ -23,6 +25,7 @@ function objectToConfigString(obj) {
         .join("\n")
 }
 
+
 const privateIPAddress = getPrivateIPAddress()
 if (privateIPAddress === null){
     throw new Error("A private IP address could not be found across your network interfaces. This suggests that you are not connected to a network at all, so double check your WiFi / Ethernet cable and try again.")
@@ -31,11 +34,24 @@ if (privateIPAddress === null){
 const FRONTEND_PORT = 3000
 const BACKEND_PORT = 5000
 
-// create-react-app uses dotenv on the backend, but will ignore any env var that doesn't start with `REACT_APP_`
+const FRONTEND_URL = `http://${privateIPAddress}:${FRONTEND_PORT}`
+const BACKEND_URL = `http://${privateIPAddress}:${BACKEND_PORT}`
+
+
+// generates a QR Code asset to be displayed on the frontend ( so you can easily access the site from your phone )
+const QRCode = require('qrcode')
+const QRCodeFilepath = path.join(__dirname, "frontend", "public", "app-qrcode.png")
+
+// saves the QR code to the file
+QRCode.toFile(QRCodeFilepath, FRONTEND_URL)
+
+
+
+// create-react-app uses dotenv behind the scenes, but will ignore any env var that doesn't start with `REACT_APP_`
 frontendEnvVars = {
     HOST: privateIPAddress,
     PORT: FRONTEND_PORT,
-    REACT_APP_BACKEND_URL: `http://${privateIPAddress}:${BACKEND_PORT}`,
+    REACT_APP_BACKEND_URL: BACKEND_URL,
     //BROWSER: "none" // uncomment this to stop react-scripts from opening a new window when the frontend server starts
 }
 
@@ -43,7 +59,9 @@ backendEnvVars = {
     PRIVATE_IP_ADDR: privateIPAddress,
     FRONTEND_PORT: FRONTEND_PORT,
     BACKEND_PORT: BACKEND_PORT,
+    BACKEND_URL: BACKEND_URL
 }
+
 
 // writes the data to .env files located at the root directories of the frontend and backend
 const frontendEnvPath = path.join(__dirname, "frontend", ".env")
@@ -53,4 +71,3 @@ fs.writeFile(frontendEnvPath, objectToConfigString(frontendEnvVars))
     .then("The frontend .env file was updated successfully.")
 fs.writeFile(backendEnvPath, objectToConfigString(backendEnvVars))
     .then("The backend .env file was updated successfully.")
-
