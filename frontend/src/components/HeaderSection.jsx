d348787795c93cbe50da8f672b0fef5221cc7bf0
omgardner <36763538+omgardner@users.noreply.github.com>
2023-07-05 14:26:25 +1000

    Added the QR Code modal and display button to the app

--- frontend/src/components/HeaderSection.jsx
+++ frontend/src/components/HeaderSection.jsx
@@ -1,16 +1,28 @@
-import { Card, Typography, CardContent } from '@mui/material';
+import { Card, Typography, CardContent, Button, Grid } from '@mui/material';
+import { useState } from 'react';
+import QRCodeDialog from './QRCodeDialog';
 
 const HeaderSection = () => {
+    const [dialogOpen, setDialogOpen] = useState(false)
     return (
-        <Card>
-            <CardContent sx={{ flex: '1 0 auto' }}>
-                <Typography component="div" variant="h5">
-                    Home Data Sharing
-                </Typography>
-                <Typography variant="subtitle1" color="text.secondary" component="div">
-                    Transfer files between LAN devices
-                </Typography>
-            </CardContent>
+        <Card padding={2}>
+            <Grid container sx={{ direction: 'row', alignItems: 'center', justifyContent: 'center'}}
+                columns={{ xs: 8, sm: 8, md: 8, lg: 12 }}>
+                <Grid item xs={6} padding={2}>
+                    <Typography component="div" variant="h5">
+                        Home Data Sharing
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                        Transfer files between LAN devices
+                    </Typography>
+                </Grid>
+                <Grid item xs={2} padding={2}>
+                    <Button variant='contained' onClick={() => setDialogOpen(true)}>Show QR Code</Button>
+                </Grid>
+                <QRCodeDialog open={dialogOpen} closeDialog={() => setDialogOpen(false)} />
+
+
+            </Grid >
         </Card>
     );
 }
